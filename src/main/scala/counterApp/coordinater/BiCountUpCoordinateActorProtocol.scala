0b6ace50951aab4275479ba138717a3dc9195740
s10myk4 <st4439@gmail.com>
2022-03-12 04:58:17 +0900

    Akka persistenceを使ってPersist FSMなSagaオーケストレーターを実装

--- src/main/scala/counterApp/coordinater/BiCountUpCoordinateActorProtocol.scala
+++ src/main/scala/counterApp/coordinater/BiCountUpCoordinateActorProtocol.scala
@@ -7,7 +7,18 @@ object BiCountUpCoordinateActorProtocol {
 
   final case class ExecCommands(num: Int, replyTo: ActorRef[ExternalReply]) extends Command
 
-  final case class AggregateReply(replies: Seq[InternalReply]) extends Command
+  final case class AggregateReply(replies: Seq[InternalReply]) extends Command {
+    def isTimeoutAll: Boolean = replies.isEmpty
+
+    def isAllSuccessful(expectedReplyNum: Int): Boolean =
+      replies.forall(_.isInstanceOf[SuccessfulReply]) && replies.length == expectedReplyNum
+
+    def isAllFailed: Boolean = replies.forall(_.isInstanceOf[FailedReply])
+
+    def isOneFailed: Boolean =
+      replies.headOption.exists(_.isInstanceOf[FailedReply]) && replies.tail.isEmpty
+
+  }
 
   sealed trait Reply
 
